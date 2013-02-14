450aa2483afede5ff4a6a943114fb4aba2093c71
albertef <albertef@gmail.com>
2013-02-13 19:31:15 -0800

    home page updates in dev folder

--- dev/index.php
+++ dev/index.php
@@ -13,40 +13,63 @@ include("includes/left_submenu.php");
 				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea, the first time the company is taking delivery of newbuilds of this size. The first vessel was christened the m.v. OOCL Brussels by Sponsor Mrs. Hsu Lai, Chao-Yin, the wife of Mr. Shiu, Kuang-Si, President of Mega International Commercial Bank, Taiwan. The second vessel was christened the m.v. NYK Helios by Sponsor Mrs. Nintira Sophonpanich, the wife of Mr. Chartsiri Sophonpanich, President of Bangkok Bank. Mr Andy Tung, Chief Executive Officer of OOCL, hosted the naming ceremony and thanked the Sponsors for gracing the stage to perform the formalities and in thanking Mega International Commercial Bank and Bangkok Bank for their support </p>
 				<h6><a href="">read more &raquo;</a></h6>
 				</span> </li>
+			<li class="small">
+				<h1><a href="">OOCL hits milestone with double christening at Geoje</a></h1>
+				<span> <img src="resources/images/media.jpg" alt="" />
+				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea</p>
+				<h6><a href="">read more &raquo;</a></h6>
+				</span> </li>
+			
+			<li class="small">
+				<h1><a href="">OOCL hits milestone with double christening at Geoje</a></h1>
+				<span> <img src="resources/images/media.jpg" alt="" />
+				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea</p>
+				<h6><a href="">read more &raquo;</a></h6>
+				</span> </li>
+			
+			<li class="small">
+				<h1><a href="">OOCL hits milestone with double christening at Geoje</a></h1>
+				<span> <img src="resources/images/media.jpg" alt="" />
+				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea</p>
+				<h6><a href="">read more &raquo;</a></h6>
+				</span> </li>
+			
+		</ul>
+			<ul class="newsSection">
 			<li>
 				<h1><a href="">OOCL hits milestone with double christening at Geoje Island Shipyard</a></h1>
 				<span> <img src="resources/images/media.jpg" alt="" />
-				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea, the first time the company is taking delivery of newbuilds of this size. The first vessel was christened the m.v. OOCL Brussels by Sponsor Mrs. Hsu Lai, Chao-Yin, the wife of Mr. Shiu, Kuang-Si, President of Mega International Commercial Bank, Taiwan. </p>
+				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea, the first time the company is taking delivery of newbuilds of this size. </p>
 				<h6><a href="">read more &raquo;</a></h6>
 				</span> </li>
 			<li>
 				<h1><a href="">OOCL hits milestone with double christening at Geoje Island Shipyard</a></h1>
 				<span> <img src="<?php echo $http_path_site;?>resources/images/media.jpg" alt="" />
-				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea, the first time the company is taking delivery of newbuilds of this size. The first vessel was christened the m.v. OOCL Brussels by Sponsor Mrs. Hsu Lai, Chao-Yin, the wife of Mr. Shiu, Kuang-Si, President of Mega International Commercial Bank, Taiwan. </p>
+				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea, the first time the company is taking delivery of newbuilds of this size. </p>
 				<h6><a href="">read more &raquo;</a></h6>
 				</span> </li>
 			<li>
 				<h1><a href="">OOCL hits milestone with double christening at Geoje Island Shipyard</a></h1>
 				<span> <img src="<?php echo $http_path_site;?>resources/images/media.jpg" alt="" />
-				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea, the first time the company is taking delivery of newbuilds of this size. The first vessel was christened the m.v. OOCL Brussels by Sponsor Mrs. Hsu Lai, Chao-Yin, the wife of Mr. Shiu, Kuang-Si, President of Mega International Commercial Bank, Taiwan. </p>
+				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea, the first time the company is taking delivery of newbuilds of this size.</p>
 				<h6><a href="">read more &raquo;</a></h6>
 				</span> </li>
 			<li>
 				<h1><a href="">OOCL hits milestone with double christening at Geoje Island Shipyard</a></h1>
 				<span> <img src="<?php echo $http_path_site;?>resources/images/media.jpg" alt="" />
-				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea, the first time the company is taking delivery of newbuilds of this size. The first vessel was christened the m.v. OOCL Brussels by Sponsor Mrs. Hsu Lai, Chao-Yin, the wife of Mr. Shiu, Kuang-Si, President of Mega International Commercial Bank, Taiwan. </p>
+				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea, the first time the company is taking delivery of newbuilds of this size.</p>
 				<h6><a href="">read more &raquo;</a></h6>
 				</span> </li>
 			<li>
 				<h1><a href="">OOCL hits milestone with double christening at Geoje Island Shipyard</a></h1>
 				<span> <img src="<?php echo $http_path_site;?>resources/images/media.jpg" alt="" />
-				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea, the first time the company is taking delivery of newbuilds of this size. The first vessel was christened the m.v. OOCL Brussels by Sponsor Mrs. Hsu Lai, Chao-Yin, the wife of Mr. Shiu, Kuang-Si, President of Mega International Commercial Bank, Taiwan. </p>
+				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea, the first time the company is taking delivery of newbuilds of this size.</p>
 				<h6><a href="">read more &raquo;</a></h6>
 				</span> </li>
 			<li>
 				<h1><a href="">OOCL hits milestone with double christening at Geoje Island Shipyard</a></h1>
 				<span> <img src="<?php echo $http_path_site;?>resources/images/media.jpg" alt="" />
-				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea, the first time the company is taking delivery of newbuilds of this size. The first vessel was christened the m.v. OOCL Brussels by Sponsor Mrs. Hsu Lai, Chao-Yin, the wife of Mr. Shiu, Kuang-Si, President of Mega International Commercial Bank, Taiwan. </p>
+				<p>OOCL is proud to announce the christening of two 13,208-TEU mega vessels at a ceremonial event held at the Geoje Island Shipyard in Korea, the first time the company is taking delivery of newbuilds of this size.</p>
 				<h6><a href="">read more &raquo;</a></h6>
 				</span> </li>
 		</ul>
